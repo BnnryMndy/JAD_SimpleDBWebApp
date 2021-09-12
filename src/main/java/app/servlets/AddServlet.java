ac7fa250dc860a9f58589594be90caf635b3d4f3
Bnnrymndy@gmail.com <Bnnrymndy@gmail.com>
2021-09-12 19:44:20 +0300

    testing servlet

--- src/main/java/app/servlets/AddServlet.java
+++ src/main/java/app/servlets/AddServlet.java
@@ -1,6 +1,18 @@
 package app.servlets;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class AddServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("Method GET from AddServlet");
+//        super.doGet(req, resp);
+    }
 }
