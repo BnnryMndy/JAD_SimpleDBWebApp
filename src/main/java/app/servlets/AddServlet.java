cba531d2d644ebab77c34c3f06dbcd3b7f2b815e
Bnnrymndy@gmail.com <Bnnrymndy@gmail.com>
2021-09-12 19:37:10 +0300

    Initial commit

---
+++ src/main/java/app/servlets/AddServlet.java
@@ -0,0 +1,6 @@
+package app.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+public class AddServlet extends HttpServlet {
+}
---
+++ src/main/java/app/servlets/ListServlet.java
@@ -0,0 +1,6 @@
+package app.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+public class ListServlet extends HttpServlet {
+}
