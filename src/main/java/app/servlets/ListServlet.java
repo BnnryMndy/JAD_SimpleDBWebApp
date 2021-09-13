2ab067e8e65e592d3148b92fb7e0c0b5e68eb0df
Bnnrymndy@gmail.com <Bnnrymndy@gmail.com>
2021-09-13 14:06:51 +0300

    add ListUser Servlet

--- src/main/java/app/servlets/ListServlet.java
+++ src/main/java/app/servlets/ListServlet.java
@@ -1,15 +1,22 @@
 package app.servlets;
 
+import app.model.Model;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class ListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Model model = Model.getInstance();
+        List<String> userNames = model.getList();
+        req.setAttribute("userNames", userNames);
+
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("views/list.jsp");
         requestDispatcher.forward(req, resp);
     }
